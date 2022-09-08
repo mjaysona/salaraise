d9d93dc963489c672ccaced1755c18e872bdac65
Mark Jayson Alimboyogen <mark.jayson.alimboyogen@ing.com>
2022-09-08 16:17:18 +0800

    Fix typescript problem/s.

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin=''
+            crossOrigin=""
           />
           <link
             href="https://fonts.googleapis.com/css2?family=MuseoModerno:wght@400;600&display=swap"
